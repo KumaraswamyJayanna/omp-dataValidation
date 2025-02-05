22ca086eea5b0d5619501b30899ca357d5754779
KumaraswamyJayanna <kumaraswamy.jaya@tigeranalytics.com>
2025-02-05 09:59:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+
+OUTPUTPATH = "Test_Data/Treasury&Banking_GroundTruth.xlsx"
+GTPATH = "Test_Data/Treasury&Banking_Pipelineoutput.xlsx"
+REPORTPATH = "Reports"
+VALIDATIONREPORT = "ValidationData"
+SUMMARYREPORT = "Reports"
+cols_for_primary_key = ['File_Name', 'Product_Service_SKU_Name_Original']
+validationdata1 = "ValidationData/pipelineValidationData_result_Treasury&Banki2025-02-04.xlsx"
+validationdata2 = "ValidationData/groundtruthValidationData_result_Treasury&Banki2025-02-04.xlsx"
+
+
+OUTPUTFILE = REPORTPATH+"/final_report_output_TB.xlsx"
+# Threshold for file-level accuracy (adjust as needed)
+ACCURACYTHRESHOLD = 90
