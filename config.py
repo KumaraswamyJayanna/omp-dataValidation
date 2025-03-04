07a317a9634fdb3a4e6223b2580549435f3817b1
KumaraswamyJayanna <kumaraswamy.jaya@tigeranalytics.com>
2025-03-04 11:23:33 +0530

    [QA][LS]: integrated the code of business logics

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ OUTPUTPATH = "Test_Data/Microsoft_Pipeline_Output.xlsx"
 # copy the Groundtruth file path
 GTPATH = "Test_Data/Ground_Truth_Software_Microsoft.xlsx"
 
+# TO Check the business level logics enter the file path
+FILE_TO_CHECK_BUSINESS_LOGIC = "Test_Data/tbs_flat_file.xlsx"
+
 # Enter the key values to generate a pseudo key
 # while selecting the key columns please select the columns are text, avoid float dtype columns
 KEYS = ['Product_Service_SKU_Name_Original','Level 5 Category']
